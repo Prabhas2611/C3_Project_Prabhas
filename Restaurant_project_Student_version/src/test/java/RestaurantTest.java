562d0f19e69e9d3073eddbfe91ee3d84902d3ffb
Prabhas2611 <prabhas2611@gmail.com>
2021-06-02 00:56:25 +0530

    Part2: Solution

--- Restaurant_project_Student_version/src/test/java/RestaurantTest.java
+++ Restaurant_project_Student_version/src/test/java/RestaurantTest.java
@@ -1,9 +1,11 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.time.LocalTime;
+import java.util.List;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
